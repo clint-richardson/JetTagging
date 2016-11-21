6b15ceaf7488b7bd16f831f5a8dfdbf6c4d043f4
clint-richardson <clint.a.richardson@gmail.com>
2016-11-21 15:47:12 -0500

    typo fix

--- AnalysisTools/Run.py
+++ AnalysisTools/Run.py
@@ -8,13 +8,13 @@ df_bkg = pd.read_csv('../ConversionTools/JetInfo_QCD.csv')
 
 #add together and then shuffle
 df = pd.concat([df_sig,df_bkg])
-#ranodmize three times so that it's hoepfully really random
+#randomize three times so that it's hopefully really random
 df = df.sample(frac=1)
 df = df.sample(frac=1)
 df = df.sample(frac=1)
 
 #use the first 18k for training, last 5k for testing
 df_train = df.iloc[0:17999,:]
-df_test = df.iloc[18000,:]
+df_test = df.iloc[18000:-1,:]
 #let's try to run the tensor flow regression using only pixels, which is 400 variables since histogram was 20x20
 runTensorFlowRegression(400,0.00001,df_train,df_test)
