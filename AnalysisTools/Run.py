c37823f8aac336679204e3ea39ce7928e785aabe
clint-richardson <clint.a.richardson@gmail.com>
2016-11-21 15:41:34 -0500

    adding run file

---
+++ AnalysisTools/Run.py
@@ -0,0 +1,20 @@
+#!/usr/env/python
+import pandas as pd
+from RegressionTools import *
+
+#open csv files
+df_sig = pd.read_csv('../ConversionTools/JetInfo_TopJets.csv')
+df_bkg = pd.read_csv('../ConversionTools/JetInfo_QCD.csv')
+
+#add together and then shuffle
+df = pd.concat([df_sig,df_bkg])
+#ranodmize three times so that it's hoepfully really random
+df = df.sample(frac=1)
+df = df.sample(frac=1)
+df = df.sample(frac=1)
+
+#use the first 18k for training, last 5k for testing
+df_train = df.iloc[0:17999,:]
+df_test = df.iloc[18000,:]
+#let's try to run the tensor flow regression using only pixels, which is 400 variables since histogram was 20x20
+runTensorFlowRegression(400,0.00001,df_train,df_test)
